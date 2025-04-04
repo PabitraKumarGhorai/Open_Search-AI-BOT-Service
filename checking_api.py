350bedbe03f09b9a88ef18f782188fac6d0a4e76
Pabitra Kumar Ghorai <ghoraipabitrakumar@gmail.com>
2025-04-05 01:58:52 +0530

    Environemental Bug Fix

--- checking_api.py
+++ checking_api.py
@@ -1,13 +1,17 @@
 import openai
-import config
-openai.api_key = config.api_key
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("api_key")
+
 
 user_input = input("Please give your input: ")
 
 
 response = openai.chat.completions.create(
-model="gpt-3.5-turbo",
-messages=[{"role": "user", "content": user_input}]
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": user_input}]
 )
 #print(response)
 
